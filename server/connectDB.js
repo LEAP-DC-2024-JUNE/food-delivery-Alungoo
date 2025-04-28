880ede3acd0ae6b384c36f3c5f01db9f6caa0181
Alungoo-T <alungoo976@gmail.com>
2025-04-27 22:08:55 -0400

    deployed

--- server/connectDB.js
+++ server/connectDB.js
@@ -1,18 +1,28 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config({ path: "./.env" });
+dotenv.config();
+
 const MONGO_URL = process.env.MONGO_URL;
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URL);
+    if (!MONGO_URL) {
+      console.error("MONGO_URL is undefined!");
+      process.exit(1);
+    }
+
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to MongoDB!");
   } catch (err) {
-    console.log("MongoDB connection error", err);
-    process.exit(1); //asaahguigeer zogsooh uildel
+    console.error("MongoDB connection error", err);
+    process.exit(1);
   }
 };
+
 // const client = new MongoClient(
 //   "mongodb+srv://duluuf:LM7miFmm6q0eg0Qa@cluster1.pyncrqd.mongodb.net/"
 // );
